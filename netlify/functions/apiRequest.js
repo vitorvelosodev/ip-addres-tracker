e376400766554a056e87bf2f95ffa230642bea24
Vitor Veloso <vitor.asv@gmail.com>
2023-01-23 15:52:32 -0300

    using fetch

--- netlify/functions/apiRequest.js
+++ netlify/functions/apiRequest.js
@@ -1,13 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
 
 export const handler = async (event) => {
   const apiKey = process.env.VITE_API_KEY;
   const ip = event.queryStringParameters.ip
   const URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`;
-  const response = await axios.get(URL);
+  const response = await fetch(URL);
+  const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response.data)
+    body: JSON.stringify(data)
   }
 }
