38a6f25645034ec6c2e0f2c7c6353957d354aeb1
Vitor Veloso <vitor.asv@gmail.com>
2023-01-23 19:03:49 -0300

    changing js to ts to fix bug

--- netlify/functions/apiRequest.ts
+++ netlify/functions/apiRequest.ts
@@ -1,19 +1,17 @@
 // import fetch from 'node-fetch';
-// import axios from "axios";
+import axios from "axios";
 
-async function getInfo(apiKey) {
-  const ip = '8.8.8.8'
+async function getInfo(apiKey, ip) {
   const URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`;
-  const response = await fetch(URL);
-  const data = await response.json();
+  const { data } = await axios.get(URL);
   return data;
 }
 
 export async function handler(event, context, callback) {
   try {
     const apiKey = process.env.VITE_API_KEY;
-    // const ip = event.queryStringParameters.ip
-    const data = await getInfo(apiKey);
+    const ip = event.queryStringParameters.ip
+    const data = await getInfo(apiKey, ip);
   
     return {
       statusCode: 200,
